2100cece31fdd454edaccf868cb2c70ce65e59bf
Selva Ganesh M <selvadev2k@gmail.com>
2023-02-19 11:23:54 +0530

    resposive blog page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import Blog from '../../components/blog/Blog'
 import Categories from '../../components/categories/Categories'
-import Header from '../../components/header/Header'
 
 type Props = {}
 
